36413ee128f82b59413902a4d91b549b4959e6ef
SaiPradeepReddi <saipradeep101853@gmail.com>
2019-08-02 18:55:12 +0530

    weather web page using java script

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// api key :      c433397a33fd1958c3ed87ff227aebf6
+// api key :            82005d27a116c2880c8f0fcb866998a0
 
 //select elements
 const iconElement = document.querySelector(".weather-icon");
@@ -17,7 +17,7 @@ weather.temperature = {
 //app consts and vars
 const KELVIN = 273;
 //API key
-const key = "82005d27a116c2880c8f0fcb866998a0";
+const key = "c433397a33fd1958c3ed87ff227aebf6";
 
 //check if browser supports geolocation
 if('geolocation' in navigator){
