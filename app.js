a67115af32402a675df6ece710231618473326ca
SaiPradeepReddi <53170286+SaiPradeepReddi@users.noreply.github.com>
2020-08-14 18:32:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ function showError(error){
 
 //get weather from API provider
 function getWeather(latitude,longitude){
-    let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
+    let api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
     console.log(api);
     fetch(api)
         .then(function(response){
